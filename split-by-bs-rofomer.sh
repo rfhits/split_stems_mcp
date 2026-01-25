8893048fa922cbcfcca92ac3a0eae9d2d8c38d70
rfhits <wysj0117@gmail.com>
2026-01-25 18:49:06 +0800

    feat: add nogui requirements and split script

---
+++ split-by-bs-rofomer.sh
@@ -0,0 +1,6 @@
+python inference.py \
+    --model_type bs_roformer \
+    --config_path ckpt/bs_rofomer/BS-Rofo-SW-Fixed.yaml \
+    --start_check_point ckpt/bs_rofomer/BS-Rofo-SW-Fixed.ckpt \
+    --input_folder audio/ \
+    --store_dir seperated/
